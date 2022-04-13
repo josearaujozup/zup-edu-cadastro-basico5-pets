acf07fef73908faea9f6dcf3db593b91680d88b7
Denes <jose.araujo@zup.com.br>
2022-04-13 12:19:12 -0300

    metodo deletar no controlador de pet implmentado

--- src/main/java/br/com/zup/edu/pets/controller/PetController.java
+++ src/main/java/br/com/zup/edu/pets/controller/PetController.java
@@ -4,11 +4,15 @@ import java.net.URI;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.zup.edu.pets.model.Pet;
@@ -28,8 +32,21 @@ public class PetController {
 	ResponseEntity<Void> cadastrar(@RequestBody @Valid PetDTO request, UriComponentsBuilder uriComponentsBuilder){
 		Pet novoPet = request.paraPet();
 		
+		repository.save(novoPet);
+		
 		URI location = uriComponentsBuilder.path("/pets/{id}").buildAndExpand(novoPet.getId()).toUri();
 		
 		return ResponseEntity.created(location).build();
 	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remove(@PathVariable("id") Long idPet){
+		
+		Pet pet = repository.findById(idPet).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não existe cadastro de autor para o id informado"));
+		
+		repository.delete(pet);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 }
